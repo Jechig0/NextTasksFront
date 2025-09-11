6f058214e4bb476ec057a1d8a7b52dfcda5acf79
JESUS CHINCOA GONZALEZ <6003480@viewnext.com>
2025-09-11 14:00:23 +0200

    inputs de login y register modificados

--- auth/src/components/register.component.tsx
+++ auth/src/components/register.component.tsx
@@ -42,44 +42,65 @@ const Register = () => {
           <h2 className="text-3xl font-bold text-secondary">Registro</h2>
         </div>
         <div className="form-control mb-4">
-          <input
-            type="text"
-            className="input input-bordered w-full"
-            placeholder="Nombre completo"
-            onChange={handleChange}
-            name="fullName"
-            value={formData.fullName}
-          />
+            <label className="input input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.214 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input
+                    type="text"
+                    className="grow"
+                    placeholder="Nombre completo"
+                    onChange={handleChange}
+                    name="fullName"
+                    value={formData.fullName}
+                />
+            </label>
         </div>
         <div className="form-control mb-3">
-          <input
-            type="text"
-            className="input input-bordered w-full"
-            placeholder="Nombre Usuario"
-            onChange={handleChange}
-            name="username"
-            value={formData.username}
-          />
+            <label className="input input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.214 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input
+                    type="text"
+                    className="grow"
+                    placeholder="Nombre Usuario"
+                    onChange={handleChange}
+                    name="username"
+                    value={formData.username}
+                />
+            </label>
         </div>
         <div className="form-control mb-3">
-          <input
-            type="email"
-            className="input input-bordered w-full"
-            placeholder="Correo"
-            onChange={handleChange}
-            name="email"
-            value={formData.email}
-          />
+            <label className="input input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                </svg>
+                <input
+                    type="email"
+                    className="grow"
+                    placeholder="Correo"
+                    onChange={handleChange}
+                    name="email"
+                    value={formData.email}
+                />
+            </label>
         </div>
         <div className="form-control mb-4">
-          <input
-            type="password"
-            className="input input-bordered w-full"
-            placeholder="Contraseña"
-            onChange={handleChange}
-            name="password"
-            value={formData.password}
-          />
+            <label className="input input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                    <path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" />
+                </svg>
+                <input
+                    type="password"
+                    className="grow"
+                    placeholder="Contraseña"
+                    onChange={handleChange}
+                    name="password"
+                    value={formData.password}
+                />
+            </label>
         </div>
 
         <button className="btn btn-success w-full" onClick={handleSubmit}>Registrarse</button>
